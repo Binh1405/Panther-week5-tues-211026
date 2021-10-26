a29e152cd02cd137b3e20d4837e163a07610c2f6
Ngoc Vi Nguyen <ngocvinguyen@192.168.2.6>
2021-10-26 19:54:30 +0700

    update exercise 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import {useState} from 'react'
-import CountButton from './components/CountButton';
-import CountResult from './components/CountResult';
-import DecreaseButton from './components/DecreaseButton';
+// import CountButton from './components/CountButton';
+// import CountResult from './components/CountResult';
+// import DecreaseButton from './components/DecreaseButton';
 
 // function App() {
 //     const [count, setCount] = useState(0)
@@ -83,38 +83,40 @@ const [dataForm, setDataForm] = useState({
   confirmPassword:""
 })
 
+const {name, email, phoneNumber, password, confirmPassword} = dataForm
+
 const handleSubmit = (event) => {
   event.preventDefault()
-  const data = {name, email, phoneNumber, password}
-  console.log("data form", data)
+  console.log("dataForm", dataForm)
 }
 
-const handleName = (event) => {
-  setName(event.target.name)
+const handleOnChange = (event) => {
+  console.log("id: ", event.target.id)
+  setDataForm({...dataForm, [event.target.id]: event.target.value})
 }
-  
+
 return (
   <div className="App">
     <form onSubmit = {handleSubmit}>
 
       <label for="name"> Name: </label>
-      <input type="text" id="name" onChange={handleName}/>
+      <input type="text" id="name" onChange={handleOnChange} value={name} name="name"/>
       <br/>
 
       <label for="email"> Email: </label>
-      <input type="email" id="email" onChange={handleEmail}/>
+      <input type="email" id="email" onChange={handleOnChange} value={email} name="email"/>
       <br/>
 
       <label for="phoneNumber"> Phone number: </label>
-      <input type="tel" id="phone" onChange={handlePhone}/>
+      <input type="tel" id="phone" onChange={handleOnChange} value={phoneNumber} name="phone"/>
       <br/>
 
-      <label for ="password"> Password: </label>
-      <input type="password" id="password" onChange={handlePassword}/>
+      <label for="password"> Password: </label>
+      <input type="password" id="password" onChange={handleOnChange} value={password} name="password"/>
       <br/>
 
       <label for="confirmPassword"> Confirm password: </label>
-      <input type="password" id="confirmPassword" onChange={handleConfirm}/>
+      <input type="password" id="confirmPassword" onChange={handleOnChange} value={confirmPassword} name="confirmPassword"/>
       <br/>
 
       <button type="submit" value="Submit">Submit</button>
